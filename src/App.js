2aed17395309c4d821cee9426510cf592edcd3f6
Ugur <ugurkarimli1@gmail.com>
2022-12-15 01:40:58 +0400

    added navi

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import Navi from "./Navi";
 
 function App() {
   return (
     <div className="App">
     <h3> Hello react </h3>
+     <p> salams </p>
+     <Navi></Navi>
     </div>
   );
 }
---
+++ src/Navi.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class Navi extends Component {
+    render() {
+        return (
+            <div>
+
+                <p>
+                    Navi component
+                </p>
+
+            </div>
+        )
+    }
+}
